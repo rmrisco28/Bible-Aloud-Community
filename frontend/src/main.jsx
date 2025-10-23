71f766e28bd64e0d299b3919950adb95d711d137
라건국의 DeskTop <rmrisco28@naver.com>
2025-10-23 16:12:15 +0900

    프론트 엔드 수정

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ToastContainer } from "react_toastify";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
